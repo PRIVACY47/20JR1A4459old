3d49b72649f964672e4606def0ff4d9f50468e68
privacy47 <privacy4477@gmail.com>
2023-08-02 14:51:48 +0530

    updated frontend

--- backend/zipp/Router/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-
-const AllTrains = require("../Handlers/AllTrains");
-const SpecificTrain = require("../Handlers/SpecificTrain")
-Router.get("/trains", AllTrains);
-Router.get("/trains/:id",SpecificTrain );
-
-module.exports = Router;
